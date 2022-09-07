b251079aa220a5958fb45b437d0953df4765494d
LandiSimone <96439893+LandiSimone@users.noreply.github.com>
2022-09-07 18:09:33 +0200

    Update utils.c

--- nodes/sensors/utils.c
+++ nodes/sensors/utils.c
@@ -4,6 +4,7 @@ int sensor_rand_int(int min, int max){
     return ((rand() %(max - min + 1)) + min);
 }
 
+// messagge format
 void json_sample(char *message_buffer, size_t size, char* topic, int sample, char* unit, int id){
     memset(message_buffer, 0, size);
     sprintf(message_buffer,
